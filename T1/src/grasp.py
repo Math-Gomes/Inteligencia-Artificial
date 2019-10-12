7d0f65d0bc6865d3632360145bcdf7206e27ba67
Matheus <mathjvmf@gmail.com>
2019-10-12 19:11:33 -0300

    GRASP com condicao temporal e pasta com resultados adicionada

--- T1/src/grasp.py
+++ T1/src/grasp.py
@@ -1,9 +1,10 @@
 from statusManager import *
 from simpleDescent import simple_descent
-from random import randint, random
+from random import random
 from itertools import accumulate
+import time
 
-def greedy_random_construct(vt, t, num_best):
+def greedy_random_construct1(vt, t, num_best):
     status = [0]*len(vt)
     while True:
         sl = expand(status)
@@ -14,7 +15,7 @@ def greedy_random_construct(vt, t, num_best):
         total = sum([v for (v, _) in values])
         ratio = [(v/total, s) for (v, s) in values]
         ratio.sort()
-        rt_acc = list(accumulate([v for (v,p) in ratio]))
+        rt_acc = list(accumulate([v for (v, _) in ratio]))
         r = random()
         for (i, c) in enumerate(rt_acc):
             if r < c:
@@ -22,21 +23,45 @@ def greedy_random_construct(vt, t, num_best):
                 break
     return status
 
-def grasp(vt, t, num_iter, num_best, max_time):
+def greedy_random_construct(vt, t, num_best):
+    status = [0]*len(vt)
+    vt_ = vt.copy()
+    vt_.sort()
+    vt_.sort(key = lambda x: x[1])
+    while True:
+        vt_ = [e for e in vt_ if e[1] <= t]
+        if vt_ == []:
+            break
+        total = sum([v for (v, _) in vt_]) # Soma de todos os valores
+
+        ratio = [(v/total, (v, t_)) for (v, t_) in vt_]
+        ratio = ratio[-num_best:]
+
+        # Normaliza as chance de ratio
+        total = sum([v for (v, _) in ratio])
+        ratio = [(v/total, vt__) for (v, vt__) in ratio]
+        rt_acc = list(accumulate([v for (v, _) in ratio]))
+
+        r = random()
+        for (i, c) in enumerate(rt_acc):
+            if r < c:
+                j = vt.index(ratio[i][1])
+                items = t // get_t(vt, j)
+                status[j] += 1
+                t -= get_t(vt, j)
+                break
+    return status
+
+def grasp(vt, t, param, max_time):
+    (num_iter, num_best,) = param
     status = [0]*len(vt)
     best = status
+    timeout = time.time() + 60*max_time
     for _ in range(num_iter):
+        if time.time() > timeout:
+            break
         status = greedy_random_construct(vt, t, num_best)
-        status = simple_descent(status, vt, t)
+        # status = simple_descent(status, vt, t)
         if calc_value(status, vt) > calc_value(best, vt):
             best = status
     return best
-
-vt = [(1,3),(4,6),(5,7)]
-t = 19
-num_iter = 3
-num_best = 2
-max_time = 2
-
-s = grasp(vt, t, num_iter, num_best, max_time)
-print(s, calc_value(s, vt))
